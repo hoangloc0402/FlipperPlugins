0bd2b197a6b79b1f4b3b3741ee588b28b33ad671
Hoang Loc <1511849@hcmut.edu.vn>
2018-08-03 11:31:03 +0700

    Fix null pointer Error

--- app/src/main/java/com/hoangloc/flipperplugins/MyApplication.java
+++ app/src/main/java/com/hoangloc/flipperplugins/MyApplication.java
@@ -11,15 +11,15 @@ import com.facebook.sonar.plugins.network.NetworkSonarPlugin;
 
 public class MyApplication extends Application {
 
-    final SonarClient client = AndroidSonarClient.getInstance(this);
-    final DescriptorMapping descriptorMapping = DescriptorMapping.withDefaults();
-
     @Override
     public void onCreate() {
         super.onCreate();
         SoLoader.init(this, false);
 
         if (BuildConfig.DEBUG && SonarUtils.shouldEnableSonar(this)) {
+            final SonarClient client = AndroidSonarClient.getInstance(this);
+            final DescriptorMapping descriptorMapping = DescriptorMapping.withDefaults();
+
             client.addPlugin(new InspectorSonarPlugin(this, descriptorMapping));
             NetworkSonarPlugin networkSonarPlugin = new NetworkSonarPlugin();
             client.addPlugin(networkSonarPlugin);
